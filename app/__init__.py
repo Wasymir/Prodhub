3e62840535bf5cf3f41f63fa198864a0b273c1d2
Wasymir <wasymir@gmail.com>
2025-01-30 17:09:20 +0100

    Added detection of missing env var

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,19 @@ from dotenv import load_dotenv
 if os.getenv("NO_ENV_FILE") != "true":
     load_dotenv()
 
+expected_vars = [
+    "DATABASE_URL",
+    "HOST",
+    "PORT",
+    "ADMIN_SECRET"
+]
+
+for var in expected_vars:
+    if os.getenv(var) is None:
+        raise RuntimeError(f"Expected '{var}' environment variable to be defined.")
+
+
+
 if os.name == "nt":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
