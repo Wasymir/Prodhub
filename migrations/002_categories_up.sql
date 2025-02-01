33186ac25f8731e28c5c98aa0bc8d9157ed90e9f
Wasymir <wasymir@gmail.com>
2025-02-01 11:36:40 +0100

    reformat

--- migrations/002_categories_up.sql
+++ migrations/002_categories_up.sql
@@ -1,4 +1,5 @@
-create table categories (
+create table categories
+(
     category_id integer primary key generated always as identity,
-    name text not null unique check (char_length(name) between 1 and 128)
+    name        text not null unique check (char_length(name) between 1 and 128)
 );
--- migrations/003_products_up.sql
+++ migrations/003_products_up.sql
@@ -1,13 +1,15 @@
-create table products (
+create table products
+(
     product_id integer primary key generated always as identity,
-    name text not null unique check (char_length(name) between 1 and 128),
-    stock integer not null check ( stock >= 0 ),
-    price numeric not null check (price >= 0),
-    image text
+    name       text    not null unique check (char_length(name) between 1 and 128),
+    stock      integer not null check ( stock >= 0 ),
+    price      numeric not null check (price >= 0),
+    image      text
 );
 
-create table product_categories (
-    product_id integer references products on delete cascade,
+create table product_categories
+(
+    product_id  integer references products on delete cascade,
     category_id integer references categories on delete cascade,
     primary key (product_id, category_id)
 );
